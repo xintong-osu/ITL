4bb265acfec9223ed7b2b84ef072f6e23c83b843
tpeterka <tpeterka@609b6239-7616-4a8b-b317-616138058241>
2011-03-04 17:06:33 +0000

    completed nek example
    git-svn-id: https://svn.mcs.anl.gov/repos/itl/trunk@14 609b6239-7616-4a8b-b317-616138058241

--- src/itl.cpp
+++ src/itl.cpp
@@ -36,19 +36,28 @@ void ITL_get_data() {
 
 }
 
-// fortran wrapper
+//
+// fortran wrapper for nek5000 data
+// nelv = number of high-order elements (blocks) in this process
+// nx1, ny1, nz1 = number of grid points per block
+// vx, vy, vz: velocity data
+// xm1, xm2, xm3: geometry data
+// velocity and geometry ordered in (x, y, z, element) order, with x changing
+// fastest and element changing slowest
+//
 extern "C"
 void itl_get_data_(int *nelv, int *nx1, int *ny1, int *nz1, double *vx, double *vy, double *vz, double *xm1, double *ym1, double *zm1) {
 
   int rank; // MPI usual
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
+  // only printing rank 0 to reduce the amount of output
   if (rank == 0) {
 
     fprintf(stderr, "nelv = %d nx1 = %d ny1 = %d nz1 = %d\n", *nelv, *nx1, *ny1, *nz1);
 
     fprintf(stderr, "velocity field:\n");
-    for (int i = 0; i < *nx1 * *ny1 * *nz1; i++)
+    for (int i = 0; i < *nelv * *nx1 * *ny1 * *nz1; i++)
 	  fprintf(stderr, "[vx vy vz] = %.3lf %.3lf %.3lf at [x y z] = %.3lf %.3lf %.3lf\n", 
 		  vx[i], vy[i], vz[i], 
 		  xm1[i], ym1[i], zm1[i]);
